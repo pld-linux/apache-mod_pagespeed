40de8a4b5e9643bcf8119d429b326510161c16ed
Elan Ruusamäe <glen@pld-linux.org>
2011-10-08 10:56:47 +0000

    - autodetect latest version, detect python dep
    Changed files:
        get-source.sh -> 1.2

--- get-source.sh
+++ get-source.sh
@@ -5,23 +5,41 @@
 
 pkg=modpagespeed
 baseurl=http://modpagespeed.googlecode.com/svn
-# leave empty to use trunk
-version=0.9.17.7
+# leave empty to use latest tag, or "trunk" for trunk
+version=0.9.18.9
 spec=apache-mod_pagespeed.spec
 
 # abort on errors
 set -e
 
+# gclient needs python 2.6
+if python -c "import sys; sys.exit(sys.version[:3] > '2.6')"; then
+	echo >&2 "Need python >= 2.6 for gclient"
+	exit 1
+fi
+
 # work in package dir
 dir=$(dirname "$0")
 cd "$dir"
 
-if [ -n "$version" ]; then
-	svnurl=$baseurl/tags/$version/src
-	tarball=$pkg-$version.tar.bz2
-else
+if [ "$1" ]; then
+	version=$1
+fi
+
+if [ -z "$version" ]; then
+	echo "Looking for latest version..."
+	version=$(svn ls $baseurl/tags/ | sort -V | tail -n1)
+	version=${version#/}
+fi
+
+if [ "$version" = "trunk" ]; then
+	echo "Using trunk"
 	svnurl=$baseurl/trunk/src
 	tarball=$pkg-$(date +%Y%m%d).tar.bz2
+else
+	echo "Version: $version"
+	svnurl=$baseurl/tags/$version/src
+	tarball=$pkg-$version.tar.bz2
 fi
 
 wget -c http://src.chromium.org/svn/trunk/tools/depot_tools.tar.gz
