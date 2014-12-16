d1259ece3a204afd4d5d7b6196c33be7eda86661
Elan Ruusamäe <glen@delfi.ee>
2014-12-16 23:04:44 +0200

    cleanup more unneeded files for build

--- clean-source.sh
+++ clean-source.sh
@@ -18,12 +18,42 @@ almost_strip_dirs \
 	third_party/httpd24/ \
 	third_party/instaweb/ \
 	third_party/openssl/ \
+	third_party/chromium/src/base/third_party/superfasthash/ \
+	third_party/chromium/src/base/third_party/symbolize/ \
+	third_party/chromium/src/base/third_party/xdg_mime/ \
+	third_party/chromium/src/base/third_party/xdg_user_dirs/ \
 
 # some more unneeded files for build
-rm -r third_party/chromium/src/net
-rm -r third_party/chromium/src/chrome
 rm -r net/instaweb/rewriter/testdata
+rm -r third_party/aprutil/gen
+rm -r third_party/chromium/src/base/test
+rm -r third_party/chromium/src/chrome
+rm -r third_party/chromium/src/net
+rm -r third_party/gflags/src/windows
+rm -r third_party/giflib/windows
+rm -r third_party/google-sparsehash/src/windows
+rm -r third_party/libjpeg_turbo/src/{mac,win}
+rm -r third_party/protobuf/java
+rm -r third_party/protobuf/src/google/protobuf/testdata
+rm -r third_party/zlib/{contrib,examples,old,doc}
+rm -r third_party/zlib/{win32,msdos,nintendods,watcom,qnx,amiga}
 
-# build/linux and third_party/chromium/src/build/linux are same dirs, the latter is not usedc
+find -type d -name testdata | xargs rm -r
+find -type d -name mac | xargs rm -r
+find -type d -name win | xargs rm -r
+find -type d -name test | xargs rm -r
+find third_party -type d -name android | xargs rm -r
+
+find -type f -name '*_test.cc' | xargs rm
+find -type f -name '*_unittest.cc' | xargs rm
+
+# not using gn nor ninja
+find -name BUILD.gn | xargs rm
+
+# cleanup empty dirs
+find -type d '!' -name '.' -print0 | sort -zr | xargs -0 rmdir --ignore-fail-on-non-empty
+
+# build/linux and third_party/chromium/src/build/linux are same dirs,
+# the later is not used by build system, but used by tarball packaging
 #rm -r third_party/chromium/src/build/linux
 #third_party/chromium/src/build/linux
